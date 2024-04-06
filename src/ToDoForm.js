a4b27af1a38dc0a2b89fe17bf21600cb4578e417
jsitum <jure.situm@gmail.com>
2024-04-06 14:45:52 +0200

    dodan id i delete button

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 
 function ToDoForm() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   const handleAddTask = () => {
-    if (newTask.trim() !== "") {
-      setTasks([...tasks, newTask]);
-      setNewTask("");
-    }
+    const newTodo = {
+      id: uuidv4(),
+      task: newTask
+    };
+    setTasks([...tasks, newTodo]);
+    setNewTask("");
+  };
+
+  const handleDeleteTask = (id) => {
+    const todoList = tasks.filter((task) => task.id !== id);
+    setTasks(todoList);
+  };
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
   };
 
   return (
@@ -17,16 +30,17 @@ function ToDoForm() {
       <input
         type="text"
         value={newTask}
-        onChange={(event) => setNewTask(event.target.value)}
+        onChange={handleInputChange}
         placeholder="Enter task"
       />
       <button onClick={handleAddTask}>Add Task</button>
       <ul>
-        {tasks.map((task) => (
+        { tasks.map((task) => (
           <li key={task.id}>
             {task.task}
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
           </li> /* Ne koristimo indexes kao keys već id. Matej je rekao da je potrebno dodati: {id: nekiRandomId, task: newTask} 
-          Dakle, treba i kreirati random id.  */
+          Dakle, treba i kreirati random id. +++RIJEŠENO+++ */
         ))}
       </ul>
     </div>
