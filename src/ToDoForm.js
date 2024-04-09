21af31e10476ba7ad743264f0130024ddbeaf1d2
Dino Andrić <dino.andric1993@gmail.com>
2024-04-09 19:25:15 +0200

    zadnje

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -7,13 +7,16 @@ function ToDoForm() {
   const [newTask, setNewTask] = useState("");
 
   const handleAddTask = () => {
+    if (newTask.trim() !== ""){
     const newTodo = {
       id: uuidv4(),
       task: newTask
     };
     setTasks([...tasks, newTodo]);
-    setNewTask("");
-  };
+    setNewTask(""); } 
+    else {
+      alert("Unesite zadatak!"); 
+  }};
 
   const handleDeleteTask = (id) => {
     const todoList = tasks.filter((task) => task.id !== id);
@@ -24,26 +27,39 @@ function ToDoForm() {
     setNewTask(event.target.value);
   };
 
+  const handleCompletedTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  }; 
+  
   return (
     <div className="mainContainer">
       <h1>ToDo App</h1>
+      <div>
       <input
         type="text"
         value={newTask}
         onChange={handleInputChange}
         placeholder="Enter task"
         className="inputTask"
-      />
+      /></div>
+      <div>
       <button onClick={handleAddTask} className="addButton">Add Task</button>
-      <ul>
+      </div>
+      <div><ul>
         { tasks.map((task) => (
-          <li key={task.id}>
-            {task.task}
+          <li key={task.id} >
+             <input type="checkbox" checked={task.completed}
+              onChange={() => handleCompletedTask(task.id)} ></input>
+            <div className="task">{task.task}</div>
             <button className="deleteButton" onClick={() => handleDeleteTask(task.id)}>Delete</button>
           </li> /* Ne koristimo indexes kao keys već id. Matej je rekao da je potrebno dodati: {id: nekiRandomId, task: newTask} 
           Dakle, treba i kreirati random id. +++RIJEŠENO+++ */
         ))}
-      </ul>
+      </ul></div>
+      
     </div>
   );
 }
