b4559f62d09cdbc1718daefa2bd16c803d7bdefb
Dino Andrić <dino.andric1993@gmail.com>
2024-04-11 21:10:17 +0200

    zadnja funkcionirajuca verzija prije nego se MUI umjesao

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -4,10 +4,20 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
+// --------------------------------------------------------
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import Checkbox from '@mui/material/Checkbox';
+// import IconButton from '@mui/material/IconButton';
+// import CommentIcon from '@mui/icons-material/Comment';
 
 function ToDoForm() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+     
   useEffect(() => {
     const storedTasks = JSON.parse(localStorage.getItem('tasks'));
     if (storedTasks) {
@@ -46,26 +56,41 @@ function ToDoForm() {
     );
     setTasks(updatedTasks);
   }; 
+  
+  const handleToggleTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };  
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleAddTask();
+    }
+  }; 
 
   return (
    
     <div className="mainContainer">
       <h1>ToDo App</h1>
-      <div>
+      <div style={{display:"flex", justifyContent:"center" , alignItems:"center"}}>
       <TextField variant="outlined" color="primary" focused value={newTask}
         onChange={handleInputChange}
+        onKeyDown={handleKeyPress}  
         placeholder="Enter task"
-        className="inputTask"
-      /></div>
-      <div>
-      <Button variant="contained" color="primary" style={{margin: "35px"}} onClick={handleAddTask} className="addButton"><ControlPointIcon/>Add Task</Button>
+        style={{width:"300px"}}
+      />
+      <Button variant="contained" color="primary" style={{marginLeft: "30px"}} onClick={handleAddTask} className="addButton"><ControlPointIcon/>Add Task</Button>
       </div>
       <div><ul>
         { tasks.map((task) => (
           <li key={task.id}>
-             <input type="checkbox" checked={task.completed}
+            <div onClick={() => handleToggleTask(task.id)} className={task.completed ? "completed" : ""}> 
+              <input type="checkbox" checked={task.completed}
               onChange={() => handleCompletedTask(task.id)} ></input>
-            <div className={`"task" ${task.completed ? "completed" : ""}`}>{task.task}</div>
+              <label className={`"task" ${task.completed ? "completed" : ""}`}>{task.task}</label>
+            </div>
             <Button variant="outlined" color="error" style={{margin: "10px 15px"}} onClick={() => handleDeleteTask(task.id)}><DeleteIcon />Delete</Button>
           </li> /* Ne koristimo indexes kao keys već id. Matej je rekao da je potrebno dodati: {id: nekiRandomId, task: newTask} 
           Dakle, treba i kreirati random id. +++RIJEŠENO+++ */
@@ -76,4 +101,52 @@ function ToDoForm() {
   );
 }
 
+
+// return (
+   
+//   <div className="mainContainer">
+//     <h1>ToDo App</h1>
+//     <div style={{display:"flex", justifyContent:"center" , alignItems:"center"}}>
+//     <TextField variant="outlined" color="primary" focused value={newTask}
+//       onChange={handleInputChange}
+//       onKeyDown={handleKeyPress}  
+//       placeholder="Enter task"
+//       style={{width:"300px"}}
+//     />
+//     <Button variant="contained" color="primary" style={{marginLeft: "30px"}} onClick={handleAddTask} className="addButton"><ControlPointIcon/>Add Task</Button>
+//     </div>
+//     <div><List sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}>
+//       { tasks.map((task) => (
+//         <ListItem key={task.id}>
+          {/* -------------------------------------------------------------------------------------------------------------------- */}
+          {/* <div onClick={() => handleToggleTask(task.id)} className={task.completed ? "completed" : ""}> 
+            <input type="checkbox" checked={task.completed}
+            onChange={() => handleCompletedTask(task.id)} ></input>
+            <label className={`"task" ${task.completed ? "completed" : ""}`}>{task.task}</label>
+          </div>
+          <Button  variant="outlined" color="error" style={{margin: "10px 15px"}} onClick={() => handleDeleteTask(task.id)}><DeleteIcon />Delete</Button> */}
+          {/* -------------------------------------------------------------------------------------------------------------- */}
+          {/* <ListItemButton role={undefined} onClick={handleToggleTask(task.id)} dense>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={task.completed}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': task.id }}
+                />
+              </ListItemIcon>
+              <ListItemText id={task.id} primary={task.task} />
+            </ListItemButton>
+        </ListItem> /* 
+      Ne koristimo indexes kao keys već id. Matej je rekao da je potrebno dodati: {id: nekiRandomId, task: newTask}  */}
+//         Dakle, treba i kreirati random id. +++RIJEŠENO+++ */
+//       ))}
+//     </List></div>
+    
+//   </div>
+// );
+// }
+
 export default ToDoForm;
+
